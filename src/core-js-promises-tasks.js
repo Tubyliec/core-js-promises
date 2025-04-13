f19f5317089ce8cb5b170c6354e65b59d966585f
tubyliec <antikruk@vivaldi.net>
2025-04-13 14:18:38 +0300

    feat: add sixth task solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -111,8 +111,14 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const result = Promise.all(
+    promises.map((element) =>
+      element.then((resolve) => resolve).catch(() => null)
+    )
+  );
+
+  return result;
 }
 
 /**
